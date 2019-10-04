aa9c04c3c8d249391ca61221f26141a8e5f0c9ab
MicahJank <MicahJank@users.noreply.github.com>
2019-10-04 16:16:36 -0500

    created an axiosWithAuth utility

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import useForm from '../utils/useForm.js';
 
 const Login = () => {
+    const [loginInfo, handleChanges, clearForm] = useForm();
+
+    const submitHandler = e => {
+        e.preventDefault();
+        console.log(loginInfo);
+        clearForm();
+    }
+
   return (
-    <div>
-       Hello World 
-    </div>
+    <form onSubmit={submitHandler}>
+        <input 
+            type='text'
+            name='userName'
+            value={loginInfo.userName || ''}
+            onChange={handleChanges}
+        />
+        <input 
+            type='password'
+            name='userPass'
+            value={loginInfo.userPass || ''}
+            onChange={handleChanges}
+        />
+        <button>Log In</button>
+    </form>
   );
 };
 export default Login;
