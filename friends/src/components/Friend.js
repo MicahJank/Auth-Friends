6719386ef7095eecd679f95946583c76e67c7bff
MicahJank <MicahJank@users.noreply.github.com>
2019-10-07 11:54:47 -0500

    you can now edit the friends

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { axiosWithAuth } from '../utils/axiosWithAuth.js';
+import useForm from '../utils/useForm.js';
 import { useDispatch } from 'react-redux';
 
 const Friend = ({ friendData }) => {
-
+    const [editing, setEditing] = useState(false);
+    const [editInfo, handleChanges, clearForm] = useForm();
     const dispatch = useDispatch();
 
     const deleteFriend = (e) => {
@@ -27,15 +29,61 @@ const Friend = ({ friendData }) => {
             });
     }
 
-  return ( 
-    <div className='friend-container'> 
-        <h1>{friendData.name}</h1>
-        <div>
-            <h3>{friendData.age} years old</h3>
-            <h3>{friendData.email}</h3>
+    const toggleEditing = () => {
+        setEditing(!editing);
+    }
+
+    const applyChanges = () => {
+        toggleEditing();
+        dispatch({ type: "GET_FRIENDS_PENDING"})
+        axiosWithAuth().put(`/friends/${friendData.id}`, editInfo)
+            .then(res => {
+                dispatch({
+                    type: "GET_FRIENDS_SUCCESS",
+                    payload: res.data
+                })
+            })
+            .catch(err => {
+                dispatch({
+                    type: "GET_FRIENDS_FAIL",
+                    payload: err
+                })
+            })
+    }
+
+    return ( 
+        <div className='friend-container'>
+            <label hidden={editing} className={'friend name'}>{friendData.name}</label>
+            <input hidden={!editing} className={`friend name`}
+                type='text'
+                name='name'
+                value={editInfo.name || friendData.name}
+                onChange={handleChanges}
+             />
+            <label hidden={editing} className='friend age'>{friendData.age}</label> 
+            <input hidden={!editing} className={`friend age`}
+                type='text'
+                name='age'
+                value={editInfo.age || friendData.age}
+                onChange={handleChanges}
+            />
+            <label hidden={editing} className='friend email'>{friendData.email}</label>  
+            <input hidden={!editing} className={`friend email`}
+                type='email'
+                name='email'
+                value={editInfo.email || friendData.email}
+                onChange={handleChanges}
+            />
+            <div className='buttons'>
+                <button className='delete-friend' onClick={deleteFriend}>Delete</button>
+                <button onClick={toggleEditing} hidden={editing} className='edit-friend'>Edit</button>
+                <button onClick={applyChanges} hidden={!editing} className='apply-changes'>Apply</button> 
+            </div>
+           
         </div>
-        <button onClick={deleteFriend}>Delete</button> 
-    </div>
-  )
+      )
 };
+
+
+
 export default Friend;
