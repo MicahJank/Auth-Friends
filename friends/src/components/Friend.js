158e9a457be3c77ace40867613c8c96ee070c4c5
MicahJank <MicahJank@users.noreply.github.com>
2019-10-06 21:31:13 -0500

    added a delete ability to the friends list

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
+import { useDispatch } from 'react-redux';
+
 const Friend = ({ friendData }) => {
+
+    const dispatch = useDispatch();
+
+    const deleteFriend = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: "GET_FRIENDS_PENDING"
+        })
+        axiosWithAuth().delete(`/friends/${friendData.id}`)
+            .then(res => {
+                dispatch({
+                    type: "GET_FRIENDS_SUCCESS",
+                    payload: res.data
+                })
+            })
+            .catch(err => {
+                dispatch({
+                    type: "GET_FRIENDS_FAIL",
+                    payload: err
+                })
+            });
+    }
+
   return ( 
-    <div> 
+    <div className='friend-container'> 
         <h1>{friendData.name}</h1>
         <div>
             <h3>{friendData.age} years old</h3>
             <h3>{friendData.email}</h3>
-        </div> 
+        </div>
+        <button onClick={deleteFriend}>Delete</button> 
     </div>
   )
 };
